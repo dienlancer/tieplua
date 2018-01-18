024a740ed342eedf9918a570ef1d43eb306febaa
trietnk01 <trietnk01@gmail.com>
2018-01-18 23:07:03 +0700

    cập nhật

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -8,16 +8,16 @@
 			$module=getPage("trung-tam-widget");
 			if(count($module) > 0){
 				$fullname=substr($module["fullname"],0,50);
-					$featuredImg=asset('upload/'.$module['image']);
-					$permalink=route('frontend.index.index',[$module['alias']]);	
-					$intro=substr($module["intro"], 0,250) ;	
-					$content=$module['content'];
+				$featuredImg=asset('upload/'.$module['image']);
+				$permalink=route('frontend.index.index',[$module['alias']]);	
+				$intro=substr($module["intro"], 0,250) ;	
+				$content=$module['content'];
 				?>		
 				<div class="margin-top-40">
 					<h1 class="brabel"><?php echo $fullname; ?></h1>
 					<div class="tialia margin-top-15">
 						<?php 						
-							echo $content;					
+						echo $content;					
 						?>
 					</div>		
 				</div>										
@@ -32,16 +32,16 @@
 			$module=getPage("about-us-widget");
 			if(count($module) > 0){
 				$fullname=substr($module["fullname"],0,50);
-					$featuredImg=asset('upload/'.$module['image']);
-					$permalink=route('frontend.index.index',[$module['alias']]);	
-					$intro=substr($module["intro"], 0,250) ;
-					$content=$module['content'];				
+				$featuredImg=asset('upload/'.$module['image']);
+				$permalink=route('frontend.index.index',[$module['alias']]);	
+				$intro=substr($module["intro"], 0,250) ;
+				$content=$module['content'];				
 				?>			
 				<div class="margin-top-40">
 					<h1 class="brabel"><?php echo $fullname; ?></h1>
 					<div class="tialia margin-top-15">
 						<?php 						
-							echo $content;					
+						echo $content;					
 						?>
 					</div>	
 				</div>									
@@ -83,9 +83,9 @@
 		<div class="col-lg-3">
 			<div class="margin-top-40">
 				<h1 class="brabel">Fanpage</h1>
-			<div class="tialia margin-top-15">
-				<div class="fb-page" data-href="https://www.facebook.com/gonguyenlieumy/" data-tabs="timeline" data-width="250" data-height="200" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/gonguyenlieumy/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/gonguyenlieumy/">Công ty tnhh Greenecolife</a></blockquote></div>
-			</div>	
+				<div class="tialia margin-top-15">
+					<div class="fb-page" data-href="https://www.facebook.com/gonguyenlieumy/" data-tabs="timeline" data-width="250" data-height="200" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/gonguyenlieumy/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/gonguyenlieumy/">Công ty tnhh Greenecolife</a></blockquote></div>
+				</div>	
 			</div>						
 		</div>
 	</div>
