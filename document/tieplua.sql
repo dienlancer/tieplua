5a47e01fad8197fc8d3d5de42beb976938b77532
trietnk01 <trietnk01@gmail.com>
2018-02-23 08:42:25 +0700

    cập nhật

--- document/tieplua.sql
+++ document/tieplua.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 22, 2018 lúc 02:29 PM
+-- Thời gian đã tạo: Th2 23, 2018 lúc 02:40 AM
 -- Phiên bản máy phục vụ: 10.1.29-MariaDB
 -- Phiên bản PHP: 7.0.26
 
