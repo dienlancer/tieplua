13c302df5698c2448203a73192c4bf4cf685ceee
trietnk01 <trietnk01@gmail.com>
2018-02-24 12:55:16 +0700

    cap nhat

--- document/tieplua.sql
+++ document/tieplua.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 23, 2018 lúc 08:32 PM
+-- Thời gian đã tạo: Th2 24, 2018 lúc 06:51 AM
 -- Phiên bản máy phục vụ: 10.1.29-MariaDB
 -- Phiên bản PHP: 7.0.26
 
