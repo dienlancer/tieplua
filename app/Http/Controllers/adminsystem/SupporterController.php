d70c229bd0e048f33edfa569d2b371ef3e156f6e
trietnk01 <trietnk01@gmail.com>
2018-04-08 16:43:25 +0700

    cập nhật

--- app/Http/Controllers/adminsystem/SupporterController.php
+++ app/Http/Controllers/adminsystem/SupporterController.php
@@ -31,7 +31,7 @@ class SupporterController extends Controller {
           return view("adminsystem.".$this->_controller.".list",compact("controller","task","title","icon","arrDonation")); 
         }
         else{
-          return view("adminsystem.no-access");
+          return view("adminsystem.no-access",compact('controller'));
         }
   	}	    
   	public function loadData(Request $request){             
@@ -72,7 +72,7 @@ class SupporterController extends Controller {
         $arrDonation=DonationModel::select("id","fullname")->orderBy("sort_order","asc")->get()->toArray();     
         return view("adminsystem.".$this->_controller.".form",compact("arrRowData","controller","task","title","icon","arrPaymentMethod","arrDonation"));
         }else{
-          return view("adminsystem.no-access");
+          return view("adminsystem.no-access",compact('controller'));
         }        
     }
      public function save(Request $request){
